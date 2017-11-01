0d7e1c968be3d41e4ba34bc3547c1fee1ba3760c
raluandreea <olariralu@gmail.com>
2017-11-02 01:27:42 +0200

    changes on Sing in page and some tries on connecting to DB

--- MyPhotographer_PHP/DBconnection.php
+++ MyPhotographer_PHP/DBconnection.php
@@ -1,9 +1,25 @@
 <?php
+$username = "root";
+$password = "";
+$hostname = "localhost"; 
 
-	$connection = mysql_connect('localhost','root','','myphotographer_db');
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password) 
+ or die("Unable to connect to MySQL");
+echo "Connected to MySQL<br>";
 
-	if(!$connection){
-		die(mysqli_connect_error());
-	}
-	
+//select a database to work with
+$selected = mysql_select_db("myphotographer_db",$dbhandle) 
+  or die("Could not select myphotographer_db");
+
+//execute the SQL query and return records
+// $result = mysql_query("SELECT id, nume,prenume,username,email FROM register");
+
+//fetch tha data from the database 
+// while ($row = mysql_fetch_array($result)) {
+	//display the results
+   // echo "ID:".$row{'id'}."<br>"."Last Name: ".$row{'nume'}."<br>"." First Name: ".$row{'prenume'}."<br>"."Username: ".$row('username')."<br>"." Email: ".$row('email')."<br>";
+// }
+//close the connection
+mysql_close($dbhandle);
 ?>
